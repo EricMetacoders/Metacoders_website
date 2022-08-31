ca5794bacd0d2a3a150cffecb94c452e9559fc35
KHOI VU <khoivudev@gmail.com>
2022-08-31 14:05:51 +0700

    add scroll down progress animation

--- public/js/progressSteps.js
+++ public/js/progressSteps.js
@@ -1,3 +1,48 @@
+$("#first").one("inview", function (event, isInView) {
+  let pixel = 0;
+  let lastScrollTop = 0;
+
+  // Event is triggered once the element becomes visible in the browser's viewport, and once when it becomes invisible
+  if (isInView) {
+    if (pixel === 0) {
+      // document.body.style.overflow = "hidden";
+    }
+    $(window).scroll(function (event) {
+      var st = $(this).scrollTop();
+      if (st > lastScrollTop) {
+        // downscroll code
+        pixel++;
+        if (pixel === 20) {
+          $("#nprogress-bar").val("25");
+          $(".m-progress").css("height", "25%");
+          $(".second").addClass("border-change");
+          $("#second").addClass("process-content-active");
+        }
+        if (pixel === 35) {
+          $("#nprogress-bar").val("50");
+          $(".m-progress").css("height", "50%");
+          $(".third").addClass("border-change");
+          $("#third").addClass("process-content-active");
+        }
+        if (pixel === 40) {
+          $("#nprogress-bar").val("75");
+          $(".m-progress").css("height", "75%");
+          $(".fourth").addClass("border-change");
+          $("#fourth").addClass("process-content-active");
+        }
+        if (pixel === 55) {
+          $("#nprogress-bar").val("100");
+          $(".m-progress").css("height", "100%");
+          $(".five").addClass("border-change");
+          $("#five").addClass("process-content-active");
+        }
+      } else {
+      }
+      lastScrollTop = st;
+    });
+  }
+});
+
 $(".first").click(() => {
   $("#nprogress-bar").val("0");
   $(".m-progress").css("height", "0%");
